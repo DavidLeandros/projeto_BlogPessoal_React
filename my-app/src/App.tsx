b3f8b6e17ced0dedf2e31aa004253e8af337f2f5
David Leandro <dleandrosilva@hotmail.com>
2022-09-08 21:55:13 -0300

    @DavidLeandros [Projeto Blog Pessoal em React - Inicializaçao até criação do componente cadastro]

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from './paginas/home/Home';
+import Footer from './components/estaticos/footer/Footer';
+import Navbar from './components/estaticos/navbar/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
